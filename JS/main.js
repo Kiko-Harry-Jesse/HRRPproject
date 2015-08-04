e498b9d4d7c6a1b84ee9f446a3f403a78434f2ed
Harry DiFrancesco <hcdifrancesco@gmail.com>
2015-08-04 16:25:31 -0600

    js file changes

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,25 @@
-$(document).ready(function(){
 
-	
+$(document).ready(function(){
+	var add = {};
+	//on click function select sport/year
+	$("button").on("click",function() {
+		var year = $(".input").val();
+		
+var athletes = $(".input").val().get("http://api.espn.com/v1/sports/baseball/mlb/athletes")
+		
+		
+		//get all athletes for nba season.
+	}
+
+
+
 }
 
 //get requests: retrieve data we want. 
 
 //on click function.
 //
+$.get("http://api.espn.com/v1/sports/baseball/mlb/athletes", function(data){
+console.log(data);	
+});
+
