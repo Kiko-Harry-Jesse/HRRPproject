02e5e661bd05b35b14336fa649c74c3145b8b9bb
Harry DiFrancesco <hcdifrancesco@gmail.com>
2015-08-07 15:24:46 -0600

    Jquery now working

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    var add = {};
-    var date = '121713' // $("#date").val();
-        //for games with date return stats. 
+    
+    
+    //for games with date return stats. 
     var storePlayerIds;
     $.post("https://probasketballapi.com/players", {
         'api_key': 'IN72DnQLG0VZ3EWCoPaOfUXtrSsmJq68',
@@ -10,19 +10,29 @@ $(document).ready(function() {
         storePlayerIds = JSON.parse(data);
 
 
-        console.log(storePlayerIds);
-
+    });
+   
+    $('.submit').on("click", function (event) {
+    	
+    	var playerName = [];
+    	event.preventDefault();
+    	var date = $(".date").val();
+    	 console.log(date);
 
         $.post("https://probasketballapi.com/games", {
             'api_key': 'IN72DnQLG0VZ3EWCoPaOfUXtrSsmJq68',
             'date': date,
         }, function(data) {
+
             var gamesObj = JSON.parse(data);
             //loop through the gamesObj which contains their dates in order
             //to find the stats for each game
             var numberOfGames = 0;
             for (var key in gamesObj) {
                 numberOfGames++;
+            } 
+            if (numberOfGames === 0) {
+            	alert("No Games Played on that Day!")
             }
             var gamesFetched = 0;
             var playersOffRating = {};
@@ -34,7 +44,7 @@ $(document).ready(function() {
                 }, function(data) {
                     gamesFetched++;
                     var parsedObject = JSON.parse(data);
-                    console.log(parsedObject);
+                    // console.log(parsedObject);
 
                     //loop through parsedObject which is an object with all 
                     //the game_ids and extra stats to create key/value pairs 
@@ -75,65 +85,46 @@ $(document).ready(function() {
                                 //if current value in playersOffRating > min, replace
                             }
                         }
-                        var playerName = [];
+
 
 
                         for (var i = 0; i < topFive.length; i++) {
                             for (var j = 0; j < storePlayerIds.length; j++) {
                                 if (storePlayerIds[j].player_id.toString() === topFive[i][0]) {
                                     playerName.push(storePlayerIds[j].player_name);
-                                    //get the name of that player
-
 
                                 }
 
-                                //get player name and  
-
                             }
-                        }
 
+                        };
+                        // console.log(playerName);
+                        $(".players").empty();
 
-                        //look up player_id in stored Player IDs
+                         
+                         for (var i = 0; i < playerName.length; i++) {
+
+						 $(".players").prepend("<p>" + playerName[i]+ "</p>");
+
+                    };
 
                     }
+                    
+
+                   
 
                 });
+            };
 
-            }
+
+
         });
-
-
 
 
     });
 
 });
 
-//for each game_id put 	
-
-
-
-
-
-// //base logic: 
-// //1.date >> game_ids
-// //2.game_ids >> player_ids
-// //3.player ids >> position???
-// //4.position >> Offensive rating. 
-
-// $.post("https://probasketballapi.com/stats/players", {
-// 	'api_key': 'IN72DnQLG0VZ3EWCoPaOfUXtrSsmJq68',
-// 	'position': 'Guards'
-// }, function(data) {
-// 	console.log(JSON.parse(data));
-// });
-
-
-
-
-//restful API: to retrieve data as client, restful practice is to do it through a get request
-//standard that helps people approach/communicate. 
-//GET request is more standard >> $.get()
 
 
 
@@ -146,41 +137,3 @@ $(document).ready(function() {
 
 
 // $(".date").val();
-
-
-
-
-
-
-
-
-
-
-
-// Inititalize parse:
-// Parse.initialize(app id, my id)
-
-// Parse.Object.extend("classnamehere")
-//
-
-// 	//on click function select sport/year
-// 	$("button").on("click",function() {
-// 		var year = $(".input").val();
-
-// var athletes = $(".input").val().get("http://api.espn.com/v1/sports/baseball/mlb/athletes")
-
-
-// 		//get all athletes for nba season.
-// 	}
-
-
-
-// }
-
-// //get requests: retrieve data we want. 
-
-// //on click function.
-// //
-// $.get("http://api.espn.com/v1/sports/baseball/mlb/athletes", function(data){
-// console.log(data);	
-// });
